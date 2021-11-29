47a8599e7526c8799ed88363a64b4ef993be6f5e
adhithya-varshan <slugterrashane27@gmail.com>
2021-11-29 13:44:03 +0530

    first commit

---
+++ faculty/views.py
@@ -0,0 +1,14 @@
+from django.urls import reverse
+from django.http import HttpResponseRedirect
+from django.views.generic import TemplateView
+
+class TestPage(TemplateView):
+    template_name = 'test.html'
+
+class ThanksPage(TemplateView):
+    template_name = 'thanks.html'
+
+class HomePage(TemplateView):
+    template_name = "index.html"
+
+    
